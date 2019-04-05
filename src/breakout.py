3c6c15f9988d4ccb24308e4d80d78ceb8829a275
Nick Napora <nicholas.napora.18@cnu.edu>
2019-04-05 11:43:03 -0400

    finished week 11?

--- src/breakout.py
+++ src/breakout.py
@@ -5,6 +5,8 @@ import os
 import pygame
 from pygame.locals import *
 
+LIVES = 1
+
 global block_sprite_path_strings
 block_sprite_path_strings = ["basic_block.png", "med_block.png", "hard_block.png"]
 
@@ -86,6 +88,9 @@ class Ball(pygame.sprite.Sprite):
         self.area = screen.get_rect()
         self.vector = vector
         self.hit = 0
+        self.score = 0
+        self.lives = 5
+        self.state = 2
 
     def update(self):
         newpos = calcnewpos(self.rect, self.vector)
@@ -98,10 +103,15 @@ class Ball(pygame.sprite.Sprite):
             bl = not self.area.collidepoint(newpos.bottomleft)
             br = not self.area.collidepoint(newpos.bottomright)
             if bl and br:
-                raise SystemExit
-                # self.rect.x = 320
-                # self.rect.y = 420
-                # self.vector = (.7, 0)
+                self.lives -= 1
+                if self.lives < 1:
+                    self.state = 2
+                else:
+                    self.rect.x = 320
+                    self.rect.y = 420
+                    self.vector = (.7, 0)
+                    player.reinit()
+
             elif (tl and bl) or (tr and br):
                 angle = math.pi - angle
             elif tr and tl:
@@ -143,6 +153,7 @@ class Ball(pygame.sprite.Sprite):
             targetbrick = pygame.sprite.spritecollideany(self, bricks)
             if targetbrick is not None:
                 audio_brick_hit.play()
+                self.score += 10
 
                 tl = targetbrick.rect.collidepoint(newpos.topleft)
                 tr = targetbrick.rect.collidepoint(newpos.topright)
@@ -180,26 +191,28 @@ class Brick(pygame.sprite.Sprite):
         self.side = side
         self.speed = 0
         self.health = health
-        self.state = 'Still'
-        self.reinit()
 
-    def reinit(self):
-        self.state = "still"
-        self.movepos = [0, 0]
+
+    #     self.state = 'Still'
+    #     self.reinit()
+    #
+    # def reinit(self):
+    #     self.state = "still"
+    #     self.movepos = [0, 0]
 
 
         if self.side == "left":
-            self.rect.midleft = self.area.midleft
+            self.rect.midleft = (self.area.midleft[0], self.area.midleft[1] + 36)
         elif self.side == "center":
-            self.rect.center = self.area.center
+            self.rect.center = (self.area.center[0], self.area.center[1] + 36)
         elif self.side == "right":
-            self.rect.midright = self.area.midright
+            self.rect.midright = (self.area.midright[0], self.area.midright[1] + 36)
         elif self.side == "topleft":
-            self.rect.topleft = self.area.topleft
+            self.rect.topleft = (self.area.topleft[0], self.area.topleft[1] + 48)
         elif self.side == "topcenter":
-            self.rect.midtop = self.area.midtop
+            self.rect.midtop = (self.area.midtop[0],  self.area.midtop[1] + 48)
         elif self.side == "topright":
-            self.rect.topright = self.area.topright
+            self.rect.topright = (self.area.topright[0], self.area.topright[1] + 48)
 
 
 def main():
@@ -210,6 +223,7 @@ def main():
     pygame.display.set_caption('Breakout!')
 
     # Fill background
+    global background
     background = pygame.Surface(screen.get_size())
     background = background.convert()
     background.fill((0, 0, 0))
@@ -217,6 +231,9 @@ def main():
     # Initialize players
     global player
     player = Paddle()
+
+    global score
+    score = 0
 
     # Initialize ball
 
@@ -227,20 +244,22 @@ def main():
     # Initialize bricks
     global bricks
     bricks = pygame.sprite.Group()
-    brick0 = Brick('topleft', 2)
     brick1 = Brick('topcenter', 2)
-    brick2 = Brick('topright', 2)
     brick3 = Brick("left", 1)
     brick4 = Brick('center', 1)
     brick5 = Brick('right', 1)
 
-    bricks.add(brick0,brick1,brick2,brick3,brick4,brick5)
+    bricks.add(brick1,brick3,brick4,brick5)
 
-
-    global state
-    state = 0
+    global font
+    font = pygame.font.Font(os.path.join('assets', "font.TTF"), 36)
 
     # Initialize sprites
+    # global score_surface
+    # score_surface = font.render(str(score), False, (255, 255, 255))
+    # score_pos = score_surface.get_rect(topright=background.get_rect().topright)
+
+
     playersprites = pygame.sprite.RenderPlain((player))
     global bricksprite
     bricksprite = pygame.sprite.RenderPlain(bricks)
@@ -253,6 +272,7 @@ def main():
     # Initialize clock
     clock = pygame.time.Clock()
 
+
     #Initialize sound
     #global audio
     #audio = pygame.mixer(frequency=22050, size=-16, channels=2, buffer=4096)
@@ -262,11 +282,16 @@ def main():
     audio_brick_hit = pygame.mixer.Sound(get_sound_path("sfx_damage_hit1.wav"))
     global audio_pause
     audio_pause = pygame.mixer.Sound(get_sound_path("sfx_sounds_Blip2.wav"))
+    global audio_next_level
+    audio_next_level = pygame.mixer.Sound(get_sound_path("sfx_damage_hit10.wav"))
 
     # Event loop
     while True:
         # Make sure game doesn't run at more than 60 frames per second
         clock.tick(60)
+
+
+
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -278,10 +303,32 @@ def main():
                     player.moveleft()
                 if event.key == pygame.K_SPACE:
                     audio_pause.play()
-                    if state != 1:
-                        state = 1
+                    if ball.state == 0:
+                        ball.state = 1
                     else:
-                        state = 0
+                        ball.state = 0
+                if event.key == pygame.K_RETURN:
+                    if ball.state == 2:
+
+                        ball.lives = 5
+                        background.fill((0,0,0))
+                        score_surface = font.render(str(ball.score), False, (255, 255, 255))
+                        score_pos = score_surface.get_rect(topright=background.get_rect().topright)
+                        background.blit(score_surface, score_pos)
+                        screen.blit(background, (0, 0))
+
+                        brick1 = Brick('topcenter', 2)
+                        brick3 = Brick("left", 1)
+                        brick4 = Brick('center', 1)
+                        brick5 = Brick('right', 1)
+
+                        bricks.add(brick1, brick3, brick4, brick5)
+                        bricksprite = pygame.sprite.RenderPlain(bricks)
+
+
+
+                        ball.state = 0
+
                 if event.key == pygame.K_ESCAPE or event.key == pygame.K_q:
                     return
 
@@ -289,11 +336,49 @@ def main():
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     player.still()
 
-        screen.blit(background, player.rect, player.rect)
-        bricks.draw(background)
-        screen.blit(background, ball.rect, ball.rect)
+        if ball.state == 2:
+            if bricks:
+                bricks.empty()
+            game_over_surface = font.render("GAME OVER", False, (255, 255, 255))
+            game_over_pos = game_over_surface.get_rect(centerx=background.get_width() / 2)
+            background.fill((0,0,0))
+            background.blit(game_over_surface, game_over_pos)
+            screen.blit(background, (0,0))
 
-        if state < 1:
+        if ball.state < 1:
+            if not bricks:
+                audio_next_level.play()
+                ball.score += 100
+                bricks = pygame.sprite.Group()
+                brick0 = Brick('topleft', 2)
+                brick1 = Brick('topcenter', 2)
+                brick2 = Brick('topright', 2)
+                brick3 = Brick("left", 1)
+                brick4 = Brick('center', 1)
+                brick5 = Brick('right', 1)
+
+                bricks.add(brick0, brick1, brick2, brick3, brick4, brick5)
+
+                bricksprite = pygame.sprite.RenderPlain(bricks)
+
+            screen.blit(background, player.rect, player.rect)
+            bricks.draw(background)
+            screen.blit(background, ball.rect, ball.rect)
+
+            background.fill((0, 0, 0), rect=(background.get_rect().left, background.get_rect().top,background.get_width(),36))
+            score_surface = font.render(str(ball.score), False, (255, 255, 255))
+            score_pos = score_surface.get_rect(topright=background.get_rect().topright)
+            #background.fill((0, 0, 0), rect=score_pos)
+            background.blit(score_surface, score_pos)
+
+            lives_surface = font.render(str(ball.lives), False, (255, 255, 255))
+            lives_pos = lives_surface.get_rect(topleft=background.get_rect().topleft)
+            #background.fill((0, 0, 0), rect=score_pos)
+            background.blit(lives_surface, lives_pos)
+
+
+            screen.blit(background, (0, 0))
+
             bricksprite.update()
             playersprites.update()
             ballsprite.update()
@@ -301,6 +386,8 @@ def main():
             bricksprite.draw(screen)
             playersprites.draw(screen)
             ballsprite.draw(screen)
+
+            print(ball.score)
 
         pygame.display.flip()
 
--- src/pixel_break.py
+++ src/pixel_break.py
@@ -5,6 +5,9 @@ import os
 import pygame
 from pygame.locals import *
 
+# initializes pygame
+pygame.init()
+
 # name
 
 NAME = "PIXEL BREAK"
@@ -29,7 +32,8 @@ COLORS = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, BLACK, DARK_GRAY, GRAY, LIGH
 
 # game constants
 
-SIZE = WIDTH, HEIGHT = 800, 848
+SIZE = WIDTH, HEIGHT = 544, 580
+
 
 def load_png(name):
     """ Load image and return image object
@@ -40,7 +44,16 @@ def load_png(name):
         if image.get_alpha is None:
             image = image.convert()
         else:
-            image = image.convert_alpha()
+            # logic adapted from
+            # https://stackoverflow.com/questions/12879225/pygame-applying-transparency-to-an-image-with-alpha
+            surface = pygame.Surface(image.get_size(), depth=24)
+            key = (0, 255, 0)
+            surface.fill(key)
+            surface.set_colorkey(key)
+            surface.blit(image, (0, 0))
+            surface.set_alpha(128)
+            image = surface
+
         return image
     except pygame.error:
         print('Cannot load image:', fullname)
@@ -57,10 +70,11 @@ def calcnewpos(rect, vector):
     (dx, dy) = (z * math.cos(angle), z * math.sin(angle))
     return rect.move(dx, dy)
 
+
 def read_board(filename, group):
-    "Read plain text and add bricks to a passed group accordingly"
+    """Read plain text and add bricks to a passed group accordingly"""
     with open(os.path.join('boards', filename)) as file:
-        lines = file.read()
+        lines = file.readlines()
         for i in range(len(lines)):
             # list of strings
             split_line = lines[i].split(",")
@@ -71,42 +85,77 @@ def read_board(filename, group):
                     group.add(Brick(int(split_line[j])-1, (j * 33 + 1, i * 17 + 1)))
 
 
-
-class Bar(pygame.sprite.Sprite):
+class Ball(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.surface = load_png("doublebar.png")
-        self.rect = self.surface.get_rect()
 
-        screen = pygame.display.get_surface()
-        self.area = screen.get_rect()
-
-        self.speed = 20
-
-        # 0 == stationary, 1 == left, 2 == right
-        self.state = 0
-
-    def update(self):
-        pass
-
-
-class Ball(pygame.sprite.Sprite):
     def update(self):
         pass
     pass
 
 
-class Brick(pygame.sprite.Sprite):
-    def __init__(self, color, coordinates):
+class Bar(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.surface = load_png("quadbar.png")
+        self.rect = self.surface.get_rect()
+        screen = pygame.display.get_surface()
+        self.area = screen.get_rect()
+        self.speed = 20
+
+        # 0 == stationary, 1 == left, 2 == right
+        self.state = 0
+
+        self.reinit()
+
+    def reinit(self):
+        self.state = "still"
+        self.movepos = [0, 0]
+        self.rect.midbottom = self.area.midbottom
+
+    def update(self):
         pass
+
+
+class Brick(pygame.sprite.Sprite):
+    def __init__(self, color_code, coordinates):
+        pygame.sprite.Sprite.__init__(self)
+        self.color_code = color_code
+        self.surface = pygame.Surface(load_png("brick.png").get_size())
+        self.rect = pygame.Rect(coordinates[0], coordinates[1], self.surface.get_width(), self.surface.get_height())
+
+    def brick_draw(self):
+        self.surface.fill(COLORS[self.color_code])
+        background.blit(self.surface, self.rect)
+        self.surface = load_png("brick.png")
+        background.blit(self.surface, self.rect)
 
     def update(self):
         pass
 
 
 def main():
-    # initializes pygame
-    pygame.init()
+
+    # initialize screen
+    screen = pygame.display.set_mode(SIZE)
+    pygame.display.set_caption(NAME)
+
+    # create background surface
+    global background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill(BLACK)
+
+    # initialize title surface and do initial blit
+    # load_png creates and returns a surface object using pygame.image.load()
+    title = load_png("title.png")
+    # gets rectangle for blitting title by get_rect(), specifying center to be x centered to background
+    titlepos = title.get_rect(centerx=background.get_width() / 2)
+    background.blit(title, titlepos)
+
+    # blits background surface onto screen surface
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
 
     # initialize clock
     clock = pygame.time.Clock()
@@ -130,36 +179,22 @@ def main():
     # initialize ball and bar
     ball = Ball()
     bar = Bar()
+    brick_group = pygame.sprite.Group()
+    read_board("board1.txt", brick_group)
 
-    # initialize screen
-    screen = pygame.display.set_mode(SIZE)
-    pygame.display.set_caption(NAME)
-
-    # create background surface
-    background = pygame.Surface(screen.get_size())
-    background = background.convert()
-    background.fill(BLACK)
-
-    #
-
-    #
-
-    # initialize title surface and do initial blit
-    # load_png creates and returns a surface object using pygame.image.load()
-    title = load_png("title.png")
-    # gets rectangle for blitting title by get_rect(), specifying center to be x centered to background
-    titlepos = title.get_rect(centerx=background.get_width()/2)
-    background.blit(title, titlepos)
-
-    # blits background surface onto screen surface
-    screen.blit(background, (0, 0))
-    pygame.display.flip()
 
     # main loop
     while 1:
 
+        # defines whether entire screen needs updated
+        should_update_entire_screen = False
+
         # make sure game doesn't run at more than 60 frames per second
         clock.tick(60)
+
+        # initialize dirty rect list
+        global dirty_rects
+        dirty_rects = []
 
         for event in pygame.event.get():
             # this enables closing the window
@@ -192,9 +227,17 @@ def main():
                         state = 0
                         # fills in the background surface object
                         background.fill(BLACK)
+                        background.blit(bar.surface, bar.rect)
                         screen.blit(background, (0, 0))
+                        for brick in brick_group:
+                            brick.brick_draw()
+                        should_update_entire_screen = True
 
-        pygame.display.flip()
+
+        if not should_update_entire_screen:
+            pygame.display.update(dirty_rects)
+        else:
+            pygame.display.flip()
 
 
 
